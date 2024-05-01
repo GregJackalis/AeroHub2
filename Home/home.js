fb2bbf984efc21a1bd9b1e9c942345420cc0e6b1
TheJackal03 <gregorytsak3@gmail.com>
2024-05-01 13:30:50 +0300

    So I realised that after a user signs in, they're redirected to the home page but the login/register button is still shown while being disabled, and it wasn't a good approach because it would make users think that they haven't logged in successfully, so through this script I get an array sent to the home.html page as a parameter, and i get one of the array's elements which is the surname of the user that has just signed in.

--- Home/home.js
+++ Home/home.js
@@ -2,9 +2,18 @@ const tryBtn = document.querySelector('.tryBtn');
 const formCustBtn = document.querySelector('#getStarted');
 
 const btnParams = new URLSearchParams(window.location.search);
-const loggedStatus = btnParams.get('logged');
 
-if (loggedStatus === "true") {
+const paramArrString = btnParams.get('logged');
+const paramArr = JSON.parse(paramArrString);
+
+console.log(paramArr);
+
+if (Array.isArray(paramArr) && paramArr[0] === true) {
   tryBtn.disabled = true;
   formCustBtn.disabled = true;
+
+  const logRegBtn = document.querySelector('#getStarted');
+  logRegBtn.textContent = "Welcome back, Mr/Mrs " + paramArr[1];
 }
+
+
